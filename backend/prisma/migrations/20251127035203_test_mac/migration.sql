24f129a3ca0cd516bd48445131369e3628fbef3c
Kunal Malhotra <mkunal333@gmail.com>
2025-12-01 12:28:46 +1300

    Setup completed: Prisma config, DB migration, backend structure, and Next.js frontend project initialized

---
+++ backend/prisma/migrations/20251127035203_test_mac/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - Added the required column `passwordHash` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "approvalStatus" TEXT NOT NULL DEFAULT 'pending',
+ADD COLUMN     "passwordHash" TEXT NOT NULL;
